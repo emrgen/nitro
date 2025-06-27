39c292be0934f4acafe54d436dd07f8955a9baad
subhasis maity <hola.maity@gmail.com>
2025-06-27 13:21:20 +0530

    Update change_sorter.rs

--- src/change_sorter.rs
+++ src/change_sorter.rs
@@ -1,1 +1,18 @@
+// keep the changes in sorted order for the document
 
+// ChangeTree is a BTree like append-only structure
+// It has a simplified vector like API for efficient insertion by index.
+// Support index look up and slice api
+struct ChangeTree<V> {
+  root: Node<V>
+}
+
+enum Node<V> {
+  Branch(Branch<V>),
+  Leaf(Leaf<V>),
+}
+
+struct Branch<V> {
+  children: Vec<Node<V>>,
+  total: usize,
+}
